4e1eceeb019872530567f8c6573a648e7a699bf5
vijay3535 <31932479+vijay3535@users.noreply.github.com>
2017-09-16 12:20:22 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,16 @@
+public class Main {
+
+	public static void main(String[] args) {
+		
+		//You may test that your code works find here
+		//Please check that your code works and has no 
+		//compilation problems before to submit
+	StudentGroup gf=new StudentGroup(1);
+	Student[] s=new student();
+	s[0]=new student(25,"vijay","1996-04-27",93.65);
+	gf.setstudents(s);
+	System.out.println(gf.getstudent(0).fullname);
+	}
+	
+
+}
